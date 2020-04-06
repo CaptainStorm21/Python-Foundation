9b9e0372bd161aa452094b7c99e38ce3501c7772
captainstorm21 <tikana4@yahoo.com>
2020-04-06 01:49:03 -0700

    date and validation

---
+++ .history/9-modules_20200406013944.py
@@ -0,0 +1,14 @@
+# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+# Core modules
+import datetime
+from _datetime import date
+
+import time
+
+
+# today = datetime.date.today()
+today = date.today()
+print(today)
+
+timestamp = time.time
