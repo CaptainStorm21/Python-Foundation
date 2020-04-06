637a02620a93e99049a7656d0d829708f0946748
captainstorm21 <tikana4@yahoo.com>
2020-04-05 23:04:23 -0700

    functions

---
+++ .history/6-functions_20200405211530.py
@@ -0,0 +1,28 @@
+# A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
+
+def sayHello():
+    print('Hello World')
+    
+sayHello()
+
+def sayGoodMorning (name = 'Alfed'):
+    print('Good mornig ' + name)
+
+sayGoodMorning('Morgan')
+sayGoodMorning()
+
+# Return a value
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+print(getSum(2, 3))
+
+#increment
+def addOnetoNum(num):
+    num++
+    return num
+
+# A lambda function is a small anonymous function.
+# A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+
