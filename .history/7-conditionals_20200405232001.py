ba719419b38dc30588f2fea8359e9383c96d4776
captainstorm21 <tikana4@yahoo.com>
2020-04-05 23:58:32 -0700

    conditionals

---
+++ .history/7-conditionals_20200405232001.py
@@ -0,0 +1,25 @@
+# If/ Else conditions are used to decide to do something based on something being true or false
+
+x = 10
+y = 8
+
+# Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
+
+
+# simple if
+if x == y:
+    print(f'{x} is equal to {y}')
+    
+
+
+# Logical operators (and, or, not) - Used to combine conditional statements
+
+
+
+
+# Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
+
+
+
+
+# Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
---
+++ .history/7-conditionals_20200405235058.py
@@ -0,0 +1,50 @@
+# If/ Else conditions are used to decide to do something based on something being true or false
+
+x = 10
+y = 8
+
+# Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
+
+
+# simple if
+if x == y:
+    print(f'{x} is equal to {y}')
+
+#  If/else
+if x > y:
+    print(f'{x} is greater than {y}')
+else:
+        print(f'{x} is less than {y}')
+        
+#  If/elif/else
+if x > y:
+    print(f'{x} is greater than {y}')
+if x == y:
+    print(f'{x} is equal to {y}')
+else:
+        print(f'{x} is less than {y}')
+        
+# nested If
+if x > 2:
+    if x<=10:
+        print(f'{x} is greater than {y} and x is greater than 2')
+        
+# Logical operators (and, or, not) - Used to combine conditional statements
+if x > 2 and x <= 10:
+            print(f'{x} is greater than {y} and x is greater than 2')
+
+#not
+if not (x == y ):
+    print(f'{x} does not equal to {y}')
+
+
+
+# Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
+
+numbers = [90]
+
+if x in numbers: 
+    print(x in numbers)
+
+
+# Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
