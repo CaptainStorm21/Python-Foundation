e3af7b8bc020186991412336c8d9b61e7e3ec7a1
captainstorm21 <tikana4@yahoo.com>
2020-04-15 01:43:52 -0700

    __init__

---
+++ .history/3-OO-Python/3-__init___20200415013455.py
@@ -0,0 +1,30 @@
+# OOP
+
+class PlayerCharacter :
+    # class object attribute - it is static
+    membership = True
+    # constructor method / init method
+    def __init__(self, name='Anon', age=0):
+        if (age > 18):
+            self.name = name #attributes
+            self.age = age
+        
+    def run (self, hello):
+        print('running')
+        return 'Workout is done'
+    
+    def shout(self):
+        print(f'My name is {self.name}!')
+
+
+player1 = PlayerCharacter('Josh', 23)
+player2 = PlayerCharacter('Tony', 10)
+
+
+#blueprint of the object
+# help(player1)
+# help(list)
+
+#attributes - dynamic data - F.E,  name, age
+
+print(player1.membership)
---
+++ .history/3-OO-Python/4-classmethod-staticmethod_20200415014251.py
@@ -0,0 +1,1 @@
+4-classmethod-staticmethod.py
