99c58d6d51d603e5fd0377b090b622b1d63cdcc5
captainstorm21 <tikana4@yahoo.com>
2020-04-22 20:24:24 -0700

    reduce

--- .history/4-functional-programming/6-lambda_20200422202224.py
+++ .history/4-functional-programming/6-lambda_20200422202224.py
@@ -12,6 +12,5 @@ print(list(map(lambda item: item * 2, my_list)))
 print('filter lambda')
 print(list(filter(lambda item: item % 2 !=0, my_list)))
 
-my_list = [10, 20, 30 , 40]
 print('reduce')
 print(reduce(lambda acc, item: acc + item, my_list))
--- .history/4-functional-programming/6-lambda_20200422202338.py
+++ .history/4-functional-programming/6-lambda_20200422202338.py
@@ -12,6 +12,6 @@ print(list(map(lambda item: item * 2, my_list)))
 print('filter lambda')
 print(list(filter(lambda item: item % 2 !=0, my_list)))
 
-my_list = [10, 20, 30 , 40]
+my_list = [10, 20, 30]
 print('reduce')
 print(reduce(lambda acc, item: acc + item, my_list))
